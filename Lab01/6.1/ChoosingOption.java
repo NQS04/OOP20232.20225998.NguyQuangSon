4860788f3f38f735a509c22d32152d691c79046b
Nguy Quang Son <son.nq225998@sis.hust.edu.vn>
2024-03-13 21:41:02 +0700

    Update Answer

--- Lab01/6.1/ChoosingOption.java
+++ Lab01/6.1/ChoosingOption.java
@@ -6,6 +6,6 @@ public class ChoosingOption {
             "Do you want to change to the first class ticket?");
         JOptionPane.showMessageDialog(null, "You've chosen: " 
                     + (option == JOptionPane.YES_OPTION?"Yes":"No"));
-    System.exit(0);
+        System.exit(0);
     }
 }
