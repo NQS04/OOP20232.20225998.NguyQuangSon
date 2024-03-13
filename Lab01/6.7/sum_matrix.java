200ee397830910406337cab8bebe37c461825c76
Nguy Quang Son <son.nq225998@sis.hust.edu.vn>
2024-03-13 20:57:22 +0700

    update 6.7

--- Lab01/6.7/sum_matrix.java
+++ Lab01/6.7/sum_matrix.java
@@ -5,37 +5,39 @@ public class sum_matrix {
         Scanner read = new Scanner(System.in);
 
         //Read matrix size
-        System.out.println("Enter the size of matrix: ");
-        int size = read.nextInt();
-        int[][] matrix_1 = new int[size][size];
-        int[][] matrix_2 = new int[size][size];
-        int[][] matrix_sum = new int[size][size];
+        System.out.println("Enter the size of row : ");
+        int r = read.nextInt();
+        System.out.println("Enter the size of column : ");
+        int c = read.nextInt();
+        int[][] matrix_1 = new int[r][c];
+        int[][] matrix_2 = new int[r][c];
+        int[][] matrix_sum = new int[r][c];
         
         //Read in matrix A
         System.out.println("Enter the values of matrix 1: ");
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 matrix_1[i][j] = read.nextInt();
             }
         }
         //Read in matrix B
         System.out.println("Enter the values of matrix 2: ");
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 matrix_2[i][j] = read.nextInt();
             }
         }
 
         //Cal sum of two matrices
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 matrix_sum[i][j] = matrix_1[i][j] + matrix_2[i][j];
             }
         }
         //Print out results
         System.out.println("Sum of 2 matrices: ");
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 System.out.print(matrix_sum[i][j] +" ");
             }
             System.out.print("\n");
